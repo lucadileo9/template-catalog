711972f13944a6a94ccd9e9256c37c55e05d3b94
lucadileo9 <lucadileo70@gmail.com>
2025-04-03 19:14:33 +0200

    minor change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Button } from '@global-ui/button';
 import './globals.css';
 import { LogOut } from 'lucide-react';
 import Button2 from './components/button';
+import HomeButton from './components/button';
 
 export const metadata = {
   title: 'Template Catalog',
@@ -19,7 +20,7 @@ export default function RootLayout({
       <body>
         <main>{children}</main>
         {/* Pulsante per tornare al catalogo */}
-        <Button2 />
+        <HomeButton></HomeButton>
       </body>
     </html>
   );
