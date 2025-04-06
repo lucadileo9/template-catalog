6f276ecbd4922c78ebcce45e014343504311776e
lucadileo9 <lucadileo70@gmail.com>
2025-04-06 19:52:21 +0200

    style: home-button color

--- app/components/atoms/ButtonHome/index.tsx
+++ app/components/atoms/ButtonHome/index.tsx
@@ -7,7 +7,7 @@ import { LogOut } from "lucide-react";
 const ButtonHome: FC<ButtonHomeProps> = ({ }) => {
   return <>
       <Button
-      className="fixed bottom-4 right-4 w-12 h-12 rounded-full bg-red-500 hover:bg-red-600 shadow-md transition-colors"
+      className="fixed bottom-4 right-4 w-12 h-12 rounded-full bg-primary hover:bg-red-300 shadow-md transition-colors"
       variant="outline" 
       size="icon" 
       onClick={() => window.location.href = "/"}
