a46663eebcf9efac371a722e80cd3c3300d20505
lucadileo9 <lucadileo70@gmail.com>
2025-04-10 21:14:46 +0200

    minor change

--- app/components/molecules/SiteList/index.tsx
+++ app/components/molecules/SiteList/index.tsx
@@ -5,7 +5,7 @@ import { externalSites } from "config/externalSites";
 
 const SiteList = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-16 p-8">
       {externalSites.map((site) => (
         <CardComponent
           key={site.url}
