eae639b78c8836984cdcb959c2bec7fcba01ea37
lucadileo9 <lucadileo70@gmail.com>
2025-04-10 22:18:35 +0200

    chore(ButtonHome): change window.location to router.push

--- app/components/atoms/ButtonHome/index.tsx
+++ app/components/atoms/ButtonHome/index.tsx
@@ -3,14 +3,16 @@ import React, { FC } from "react";
 import ButtonHomeProps from "./index.types"
 import { Button } from "@global-ui/button";
 import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const ButtonHome: FC<ButtonHomeProps> = ({ }) => {
+  const router = useRouter(); 
   return <>
       <Button
       className="fixed bottom-4 right-4 w-12 h-12 rounded-full bg-primary hover:bg-red-300 shadow-md transition-colors"
       variant="outline" 
       size="icon" 
-      onClick={() => window.location.href = "/"}
+      onClick={() => router.push("/")} 
       aria-label="Torna al catalogo"
     >
       <LogOut size={24} color="white" />
